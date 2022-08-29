f803f28b6902ac837fc3561d0b27701cbe008af3
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2022-08-29 02:25:35 -0300

    Create tecnologia_service.py

---
+++ projeto/api/services/tecnologia_service.py
@@ -0,0 +1,22 @@
+from ..models import Tecnologia
+from django.http import Http404
+
+def listar_tecnologias():
+    tecnologias = Tecnologia.objects.all()
+    return tecnologias
+
+def cadastrar_tecnologia(tecnologia):
+    return Tecnologia.objects.create(nome=tecnologia.nome)
+
+def listar_tecnologia_id(id):
+    try:
+        return Tecnologia.objects.get(id=id)
+    except Tecnologia.DoesNotExist:
+        raise Http404
+
+def editar_tecnologia(tecnologia_antiga, tecnologia_nova):
+    tecnologia_antiga.nome = tecnologia_nova.nome
+    tecnologia_antiga.save(force_update=True)
+
+def remover_tecnologia(tecnologia):
+    tecnologia.delete()
